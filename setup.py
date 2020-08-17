8fb16c11478d6bf6b1246cf2ee03c2fea866df04
Andrey Proskurnev <andrey@proskurnev.ru>
2020-08-17 12:58:19 +0300

    Bump verstion 0.2.0

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup_requires = install_requires + []
 
 setup(
     name='yaost',
-    version='0.1.10',
+    version='0.2.0',
     author='Andrey Proskurnev',
     author_email='andrey@proskurnev.ru',
     packages=find_packages(exclude=['tests']),
