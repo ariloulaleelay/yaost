402ad5a2e56f6ef8dd5104a346d74b734f29099b
Andrey Proskurnev <proskurnev@yandex-team.ru>
2023-04-18 13:44:05 +0300

    Bump version to 2.0.2

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup_requires = install_requires + []
 
 setup(
     name='yaost',
-    version='2.0.2',
+    version='2.0.3',
     author='Andrey Proskurnev',
     author_email='andrey@proskurnev.ru',
     packages=find_packages(exclude=['tests']),
